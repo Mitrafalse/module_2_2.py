32232aee8eabc058f6b56c3882d6b24d186843d5
mitra <dimamarkov320@gmail.com>
2024-11-25 18:51:26 +0300

    Add files via upload

---
+++ for in .py
@@ -0,0 +1,14 @@
+first = 42
+second = 12
+third = 41
+if first == second == third :
+    print(3)
+elif first == second or first==third or second==third:
+    print(2)
+elif not first==second==third:
+    print(0)
+
+
+
+
+
